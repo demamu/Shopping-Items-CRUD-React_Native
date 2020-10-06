8a5eafab33c00fc42b13b1da0398411e12b79df4
Daniel Nigussie <demamu@users.noreply.github.com>
2020-10-06 10:38:46 -0400

    ud

--- expressRout/controllers/users.js
+++ expressRout/controllers/users.js
@@ -1,18 +1,4 @@
-const User = require('../models/User');
-
-exports.getProducts = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all Product' });
-};
-exports.getProduct = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `get a Product ${req.params.id}` });
-};
-
-exports.updateProduct = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Update Product ${req.params.id}` });
-};
-exports.deleteProduct = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Delete Product ${req.params.id}` });
-};
+const User = require("../models/User");
 
 exports.register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -24,7 +10,7 @@ exports.register = async (req, res, next) => {
   const token = user.getSignedJwtToken();
   res
     .status(200)
-    .json({ success: true, token, msg: 'User Registered Successfully' });
+    .json({ success: true, token, msg: "User Registered Successfully" });
 };
 
 exports.login = async (req, res, next) => {
@@ -32,7 +18,7 @@ exports.login = async (req, res, next) => {
   if (!email || !password) {
     return res.status(400).json({
       success: false,
-      msg: 'Please provide an email and password',
+      msg: "Please provide an email and password",
     });
   }
 
@@ -40,7 +26,7 @@ exports.login = async (req, res, next) => {
   if (!user) {
     return res.status(401).json({
       success: false,
-      msg: 'Please provide an email',
+      msg: "Please provide an email",
     });
   }
   const isMatch = await user.matchPassword(password);
@@ -48,12 +34,10 @@ exports.login = async (req, res, next) => {
   if (!isMatch) {
     return res.status(400).json({
       success: false,
-      msg: 'Invalid Credential',
+      msg: "Invalid Credential",
     });
   }
 
   const token = user.getSignedJwtToken();
-  res
-    .status(200)
-    .json({ success: true, token, msg: 'User login Successfully' });
+  res.status(200).json({ success: true, token, email, name: user.name });
 };
--- expressRout/server.js
+++ expressRout/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
+const express = require("express");
 
-const users = require('./routers/users');
-const products = require('./routers/products');
-const errorHandler = require('./middleware/error');
+const users = require("./routers/users");
+const products = require("./routers/products");
+const errorHandler = require("./middleware/error");
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 const app = express();
 app.use(express.json());
-
-app.use('/api/v1/users', users);
-app.use('/api/v1/products', products);
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+};
+app.use(logger);
+app.use("/api/v1/users", users);
+app.use("/api/v1/products", products);
 
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
